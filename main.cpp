5312ddc593e1ce98d77f07c0984b46be5a384a08
LoreSqueri <91483152+LoreSqueri@users.noreply.github.com>
2021-11-10 23:08:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, i=1, p=i, s=0, f=0;
+    cin >> n;
+    for (int i=1; i<n+2; i++) {
+
+            f = p + s;
+            p = s;
+            s = f;
+            if (f<=n) {
+            cout << f << endl;
+        }
+    }
+    return 0;
 }
